172669bf302c8076335fd8ad2373c6f58473f535
Phillip Stafford <phillip@philliprstafford.com>
2017-10-16 17:21:34 -0400

    Final solution for BookmarkServer

--- Lesson-2/7_BookmarkServer/BookmarkServer.py
+++ Lesson-2/7_BookmarkServer/BookmarkServer.py
@@ -94,8 +94,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         if name:
             if name in memory:
                 # 2. Send a 303 redirect to the long URI in memory[name].
-                #    Delete the following line.
-                # raise NotImplementedError("Step 2 isn't written yet.")
                 self.send_response(303)  # redirect via GET
                 self.send_header('Location', memory[name])
                 self.end_headers()
@@ -120,12 +118,9 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         length = int(self.headers.get('Content-length', 0))
         body = self.rfile.read(length).decode()
         params = parse_qs(body)
-
         # Check that the user submitted the form fields.
         if "longuri" not in params or "shortname" not in params:
             # 3. Serve a 400 error with a useful message.
-            #    Delete the following line.
-            # raise NotImplementedError("Step 3 isn't written yet!")
             self.send_response(400)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
@@ -137,19 +132,13 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         if CheckURI(longuri):
             # This URI is good!  Remember it under the specified name.
             memory[shortname] = longuri
-
             # 4. Serve a redirect to the root page (the form).
-            #    Delete the following line.
-            # raise NotImplementedError("Step 4 isn't written yet!")
             self.send_response(303)  # redirect via GET
             self.send_header('Location', '/')
             self.end_headers()
         else:
             # Didn't successfully fetch the long URI.
-
             # 5. Send a 404 error with a useful message.
-            #    Delete the following line.
-            # raise NotImplementedError("Step 5 isn't written yet!")
             self.send_response(404)
             self.send_header('Content-type', 'text/plain; charset=utf-8')
             self.end_headers()
